019c3401813721a75727d75f073382a7cfc6191e
Shaiyan Khan <shaiyan.h.khan@student.uts.edu.au>
2025-08-29 00:54:47 +1000

    Initial Commit

---
+++ NutriLink/NutriLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriLinkApp.swift
+//  NutriLink
+//
+//  Created by Rifath Parveen on 29/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct NutriLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
